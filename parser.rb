a37482a7aac158d372009f4b8f431b8652554282
krasno86 <krasno86@mail.ru>
2018-10-25 14:19:47 +0300

    add district saver

--- parser.rb
+++ parser.rb
@@ -4,6 +4,8 @@ require 'spreadsheet'
 require 'mysql2'
 require 'csv'
 require_relative 'tables/schools_saver'
+require_relative 'tables/districts_saver'
+require_relative 'school_hash'
 require 'date'
 
 # table1 = Spreadsheet.open '/home/krasno_o/Загрузки/1516ABS21DAYSchool.xls'
@@ -31,11 +33,16 @@ require 'date'
 csv_files_names = Dir["csv_files/*"]
 
 csv_files_names.each do |file_name|
-  if file_name == 'csv_files/1516ABS21DAYSchool.csv'
+  # if file_name == 'csv_files/1516ABS21DAYSchool.csv'
+  #   create_school_hash(file_name)
+  #
+  # end
+  if file_name == 'csv_files/1516ABS21DAYSDistrict.csv'
     index = 0
     CSV.foreach("/home/krasno_o/work/xls_test/#{file_name}") do |row|
       if index > 2
         begin
+          p row[0]
           district_number = row[0]
           district_number = "NULL" unless district_number.count("a-zA-Z1234567890") > 0
         rescue Exception => e
@@ -50,96 +57,40 @@ csv_files_names.each do |file_name|
         end
 
         begin
-          school_number = row[2]
-          school_number = "NULL" unless school_number.count("a-zA-Z1234567890") > 0
-        rescue Exception => e
-          school_number = "NULL"
-        end
-
-        begin
-          school_name = "'#{row[3].gsub("'", "\\\\'")}'"
-          school_name = "NULL" unless school_name.count("a-zA-Z1234567890") > 0
-        rescue Exception => e
-          school_name = "NULL"
-        end
-
-        begin
-          enrollments = row[4]
+          enrollments = row[2]
           enrollments = "NULL" unless enrollments.count("a-zA-Z1234567890") > 0
         rescue Exception => e
           enrollments = "NULL"
         end
 
         begin
-          absent_21_days_or_over = row[5]
+          absent_21_days_or_over = row[3]
           absent_21_days_or_over = "NULL" unless absent_21_days_or_over.count("a-zA-Z1234567890") > 0
         rescue Exception => e
           absent_21_days_or_over = "NULL"
         end
 
         begin
-          percent_absent_21_or_more_days = row[6].gsub(",", ".").to_f
-          # percent_absent_21_or_more_days = "NULL" unless percent_absent_21_or_more_days.count("a-zA-Z1234567890") > 0
+          percent_absent_21_or_more_days = row[4].gsub(",", ".").to_f
+            # percent_absent_21_or_more_days = "NULL" unless percent_absent_21_or_more_days.count("a-zA-Z1234567890") > 0
         rescue Exception => e
           percent_absent_21_or_more_days = "NULL"
         end
 
-        schools_hash = {
-          'district_number': district_number,
-          'district_name': district_name,
-          'school_number': school_number,
-          'school_name': school_name,
-          'enrollments': enrollments,
-          'absent_21_days_or_over': absent_21_days_or_over,
-          'percent_absent_21_or_more_days': percent_absent_21_or_more_days,
-          'data_source_url': "'http://www.fldoe.org/core/fileparse.php/7584/urlt/1516ABS21DAYSchool.xls'",
-          'source_as_google_spreadsheet_url': "'https://drive.google.com/open?id=1GOUnb9rtWYxnMjHwsixOXf5pZTub_5cvzMLBLMz9wMA'",
-          'll_scrape_dev_name': "'Krasno Oleg'",
-          'academic_year': "'2015-16'"
+        district_hash = {
+            'district_number': district_number,
+            'district_name': district_name,
+            'enrollments': enrollments,
+            'absent_21_days_or_over': absent_21_days_or_over,
+            'percent_absent_21_or_more_days': percent_absent_21_or_more_days,
+            'data_source_url': "'http://www.fldoe.org/core/fileparse.php/7584/urlt/1516ABS21DAYSchool.xls'",
+            'source_as_google_spreadsheet_url': "'https://drive.google.com/open?id=1GOUnb9rtWYxnMjHwsixOXf5pZTub_5cvzMLBLMz9wMA'",
+            'll_scrape_dev_name': "'Krasno Oleg'",
+            'academic_year': "'2015-16'"
         }
-        save_schools_to_db(schools_hash)
+        save_districts_to_db(district_hash)
       end
       index += 1
     end
-
   end
-
-  # if file_name == '1516ABS21DAYSDistrict.csv'
-  #   CSV.parse("/home/krasno_o/work/xls_test/#{file_name}") do |row|
-  #     begin
-  #       district_number = row[0] if row[0].length < 4
-  #       district_number = "NULL" unless district_number.count("a-zA-Z1234567890") > 0
-  #     rescue Exception => e
-  #       district_number = "NULL"
-  #     end
-  #
-  #     begin
-  #       district_name = row[0] if row[0].length < 4
-  #       district_name = "NULL" unless district_name.count("a-zA-Z1234567890") > 0
-  #     rescue Exception => e
-  #       district_name = "NULL"
-  #     end
-  #
-  #     begin
-  #       enrollments = row[0] if row[0].length < 4
-  #       enrollments = "NULL" unless school_number.count("a-zA-Z1234567890") > 0
-  #     rescue Exception => e
-  #       enrollments = "NULL"
-  #     end
-  #
-  #     begin
-  #       absent_21_days_or_over = row[0] if row[0].length < 4
-  #       absent_21_days_or_over = "NULL" unless absent_21_days_or_over.count("a-zA-Z1234567890") > 0
-  #     rescue Exception => e
-  #       absent_21_days_or_over = "NULL"
-  #     end
-  #
-  #     begin
-  #       percent_absent_21_or_more_days = row[0] if row[0].length < 4
-  #       percent_absent_21_or_more_days = "NULL" unless percent_absent_21_or_more_days.count("a-zA-Z1234567890") > 0
-  #     rescue Exception => e
-  #       percent_absent_21_or_more_days = "NULL"
-  #     end
-  #   end
-  # end
 end
