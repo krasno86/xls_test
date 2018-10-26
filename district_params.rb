0c7bd56e1612ce1051f0c983ca99a958350f5a24
krasno86 <krasno86@mail.ru>
2018-10-26 14:10:19 +0300

    add google xls url

--- district_params.rb
+++ district_params.rb
@@ -5,19 +5,19 @@ def get_district_params(file_name)
   when file_name.include?('1213')
     year = "'2012-13'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/0069362-absent1213district.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1DBmyG7_YNvj3tNtWBISSxCKMcq3Bmtau/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/17JlrbNfPb8N4flKHdsSXqeegYZnFVN7K/view?usp=sharing'"
   when file_name.include?('2013-14')
     year = "'2013-14'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/2013-14-ABS21DAYS-District-Web.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1LuVXq6jlYMm3iLHyn_7SdGCyeVLIF4FR/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1M_yyGwRpdgdGqUizQoH7nc51790q-phA/view?usp=sharing'"
   when file_name.include?('1415')
     year = "'2014-15'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/1415ABS21DAYSDistrict.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1WRxJfjoD5u9UhXfw3JhjwPKDsliIRR77/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1Nz_dH-RD1hHoz0kfwXilJzmXI_5UawEI/view?usp=sharing'"
   when file_name.include?('1516')
     year = "'2015-16'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/1516ABS21DAYSDistrict.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1LuVXq6jlYMm3iLHyn_7SdGCyeVLIF4FR/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1wXGdoa2w0qhSMyn4ykJEz2BHdSrYgNYv/view?usp=sharing'"
   end
 
   create_district_hash(file_name, year, data_source_url, source_as_google_spreadsheet_url)
--- school_params.rb
+++ school_params.rb
@@ -5,20 +5,19 @@ def get_school_params(file_name)
   when file_name.include?('1213')
     year = "'2012-13'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/0069362-absent1213.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1GFweJPwhI3F7RuFGPWtV693t86yebjHD/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1YAcsuw9YE6EFpljnbnLPDEJmcl-XDkrc/view?usp=sharing'"
   when file_name.include?('2013-14')
     year = "'2013-14'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/2013-14-ABS21DAYS-School-Web.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1Jn9kQLf3rG1uyLIXajAfmO1-QoQ88woo/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1po7RpwR7NWpOu7JHS7j45Uv6iqkhq-Mw/view?usp=sharing'"
   when file_name.include?('1415')
-    p '==================================='
     year = "'2014-15'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/1415ABS21DAYSchool.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1CGdEa1iiDxiF7r56oiGzUVpZCvec7Zxo/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/15EPo2wk0JuRUpEurgpICmQKsRS5Oi9Na/view?usp=sharing'"
   when file_name.include?('1516')
     year = "'2015-16'"
     data_source_url = "'http://www.fldoe.org/core/fileparse.php/7584/urlt/1516ABS21DAYSchool.xls'"
-    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1zkwPy5ZuufIl17aV54ya9T-lO3dJfuHz/view?usp=sharing'"
+    source_as_google_spreadsheet_url = "'https://drive.google.com/file/d/1WoR3ysUGOMhxO6UkqUCcccENEzm4fRce/view?usp=sharing'"
   end
 
   create_school_hash(file_name, year, data_source_url, source_as_google_spreadsheet_url)
