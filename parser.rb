9116a736dc14a640ea7c8feab592bc24849573e9
krasno86 <krasno86@mail.ru>
2018-11-02 12:50:55 +0200

    add driver

--- parser.rb
+++ parser.rb
@@ -3,9 +3,13 @@ require 'open-uri'
 require 'spreadsheet'
 require 'mysql2'
 require 'csv'
-require_relative 'district_params'
-require_relative 'school_params'
 require 'date'
+require 'nokogiri'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara-webkit'
+require 'selenium-webdriver'
+
 
 # table1 = Spreadsheet.open '/home/krasno_o/Загрузки/1516ABS21DAYSchool.xls'
 # sheet1 = table1.worksheet 0
@@ -20,18 +24,32 @@ require 'date'
 #   p row
 # end
 
-# old_names_arr = Dir["xls_files/*"]
-# old_names_arr.each do |old_file_name|
-#   new_file_name = "#{old_file_name.gsub("xls_files/", "").gsub(".xls", "")}.csv"
-#   %x(ssconvert ~/work/xls_test/#{old_file_name} ~/work/xls_test/csv_files/#{new_file_name})
-# end
+# profile = Selenium::WebDriver::Firefox::Profile.new
+# proxy = Selenium::WebDriver::Proxy.new(http: "23.23.23.23:3128")
+# profile.proxy = proxy
+# profile['general.useragent.override'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
+# options = Selenium::WebDriver::Firefox::Options.new(profile: profile)
+# driver = Selenium::WebDriver.for :firefox, options: options
+#
+# driver.navigate.to "http://webprod1.isbe.net/ILEARN/Content/SearchData"
+#
+# element = driver.find_element(id: '#submit')
+# p element
 
-csv_files_names = Dir["csv_files/*"]
-
-csv_files_names.each do |file_name|
-  if file_name.include?('District') || file_name.include?('district')
-    get_district_params(file_name)
-  else
-    get_school_params(file_name)
-  end
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :firefox)
 end
+Capybara.javascript_driver = :firefox
+Capybara.configure do |config|
+  config.default_max_wait_time = 10 # seconds
+  config.default_driver = :selenium
+end
+# Visit
+browser = Capybara.current_session
+driver = browser.driver.browser
+browser.visit "http://webprod1.isbe.net/ILEARN/Content/SearchData"
+browser.find('#Submit').click
+browser.find('table tbody tr:nth-child(2) a').click
+sleep(2)
+doc = Nokogiri::HTML(driver.page_source)
+p doc.css('#DistrictInfo').text
